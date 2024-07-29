4a6544849d048d124f13f535ff9925a67531189f
Abytt <abytt5823@gmail.com>
2024-07-29 11:40:38 +0530

    createpost completed

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
+
+
+
+
+
 
 const CreatePost = () => {
-  return (
-    <div>CreatePost</div>
-  )
+
+
+    const [token,setToken]=useState(sessionStorage.getItem("token"))
+
+    const [input,setInput]=useState({
+           
+        "Message":"",
+        "userId": sessionStorage.getItem("userId")
+    })
+
+    const inputHandler=(event)=>{
+        setInput({...input,[event.target.name]:event.target.value})
+    }   
+
+    const readvalues=()=>{
+        console.log(input)
+        console.log(token)
+        axios.post("http://localhost:3030/create",input,{headers:{"token":token,"Content-Type":"application/json"}}).then(
+            (response)=>{
+                console.log(response.data)
+                if (response.data.status == " Success") {
+                    alert("Post successfully")
+                    
+                } else {
+                    alert("Something went wrong !!!")
+                    
+                }
+
+        }).catch(
+            (error)=>{console.log(error)}
+        )
+    }
+
+    
+    return (
+        <div>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xxl-12">
+
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xxl-12">
+                                <label htmlFor="" className="form-label">POST A MESSAGE</label>
+                                <textarea name="Message" value={input.Message} className="form-control" onChange={inputHandler}></textarea>
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xxl-12">
+                                <button onClick={readvalues}  className="btn btn-success">Post</button>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    )
 }
 
 export default CreatePost
